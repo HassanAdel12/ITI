153efe318ba9eade265bb60c0f6352e799b1949a
HassanAdel12 <hassan.adel.hussein@gmail.com>
2024-03-13 16:58:29 +0200

    Add C# Intro and HTML 5 & CSS 3

---
+++ C# Intro/Task 1/Task 1/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            Console.WriteLine("Hello World");
+
+
+            //Task 2
+
+            Console.WriteLine("enter one char");
+            char ch;
+            char.TryParse(Console.ReadLine(), out ch);
+            int ASCII = (int)ch;
+            Console.WriteLine(ASCII);
+
+
+            //Task 3
+
+            Console.WriteLine("enter Two float number ");
+            Console.WriteLine("Enter Number 1");
+            float Num1;
+            float.TryParse(Console.ReadLine(), out Num1);
+            Console.WriteLine("Enter Number 2");
+            float Num2;
+            float.TryParse(Console.ReadLine(), out Num2);
+            Console.WriteLine("Number 1 is " + Num1+ " Number 2 is " + Num2);
+
+
+            //Task 4
+
+            Console.WriteLine("enter Two integers number ");
+            Console.WriteLine("Enter Number 1");
+            int intnum1;
+            int.TryParse(Console.ReadLine(), out intnum1);
+            Console.WriteLine("Enter Number 2");
+            int intnum2;
+            int.TryParse(Console.ReadLine(), out intnum2);
+            Console.WriteLine("Number 1 is " + intnum1 + " Number 2 is " + intnum2 +" sum is "+(intnum1+ intnum2));
+
+
+            //Task 5
+
+
+            Console.WriteLine("enter one number ");
+            int num;
+            int.TryParse(Console.ReadLine(), out num);
+            Console.WriteLine($"Hexadecimal value of {num} is {num:X}");
+
+
+            //Task 6
+
+
+            Console.WriteLine("enter dividend ");
+            int dividend;
+            int.TryParse(Console.ReadLine(), out dividend);
+            Console.WriteLine("enter divisor ");
+            int divisor;
+            int.TryParse(Console.ReadLine(), out divisor);
+
+            int quotient = dividend/divisor;
+            int remainder = dividend%divisor;
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+
+
+            //Task 7
+
+            Console.WriteLine("enter one number ");
+            int even = int.Parse(Console.ReadLine());
+
+            if(even % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+
+
+            //Task 8
+
+
+
+            Console.WriteLine("enter Three number ");
+            Console.WriteLine("Enter Number 1");
+            int number1;
+            int.TryParse(Console.ReadLine(), out number1);
+            Console.WriteLine("Enter Number 2");
+            int number2;
+            int.TryParse(Console.ReadLine(), out number2);
+            Console.WriteLine("Enter Number 3");
+            int number3;
+            int.TryParse(Console.ReadLine(), out number3);
+
+            if (number1 > number2)
+            {
+                if (number1 > number3)
+                {
+                    Console.WriteLine(number1);
+                }
+                else
+                {
+                    Console.WriteLine(number3);
+                }
+            }
+            else if (number2 > number3)
+                Console.WriteLine(number2);
+            else
+                Console.WriteLine(number3);
+
+
+            //Task 9
+
+
+            Console.WriteLine("Enter your Grade");
+            char Grade;
+            char.TryParse(Console.ReadLine(), out Grade);
+            
+            switch (Grade)
+            {
+                case 'A':
+                    Console.WriteLine("Excellent");
+                    break;
+                case 'B':
+                    Console.WriteLine("Very Good");
+                    break;
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+                case 'D':
+                    Console.WriteLine("fair");
+                    break;
+                case 'E':
+                    Console.WriteLine("failed");
+                    break;
+                default:
+                    Console.WriteLine("char is not A B C D E");
+                    break;
+            }
+
+
+            //Task 10
+
+
+            int sum =0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("total of numbers 1:100 is " + sum);
+
+
+
+            //Task 11
+
+
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(i+" * 3 = "+(i*3));
+            }
+
+
+
+            //Task 12
+
+
+            Console.WriteLine("Enter one char");
+            char c;
+            char.TryParse(Console.ReadLine(), out c);
+
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                Console.WriteLine(c + " is an alphabet.");
+            else
+                Console.WriteLine(c + " is not an alphabet.");
+
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ C# Intro/Task 2/Task 2/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Task 1
+
+            //int[,] arr1 = new int[2, 3];
+            //int[,] arr2 = new int[2, 3];
+            //int[,] result = new int[arr1.GetLength(0)+arr2.GetLength(0), arr1.GetLength(1)];
+
+            //Console.WriteLine("Enter "+(arr1.GetLength(0) * arr1.GetLength(1)) + (arr2.GetLength(0) * arr2.GetLength(1)) + " Numbers");
+            //for (int i = 0; i < arr1.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr1.GetLength(1); j++)
+            //    {
+            //        arr1[i, j] = int.Parse(Console.ReadLine());
+            //        result[i, j] = arr1[i, j];
+            //    }
+            //}
+            //for (int i = 0; i < arr2.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr2.GetLength(1); j++)
+            //    {
+            //        arr2[i, j] = int.Parse(Console.ReadLine());
+            //        result[i + arr1.GetLength(0), j] = arr2[i, j];
+            //    }
+            //}
+            //for (int i = 0; i < result.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < result.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine(result[i, j]);
+            //    }
+            //}
+
+
+
+
+            //int[,] arr1 = new int[3, 3];
+            //int[,] arr2 = new int[2, 5];
+            //int[,] result = new int[arr1.GetLength(0) + arr2.GetLength(0), arr1.GetLength(1)+ arr2.GetLength(1)];
+
+            //Console.WriteLine("Enter "+(arr1.GetLength(0)* arr1.GetLength(1))+(arr2.GetLength(0)* arr2.GetLength(1))+" Numbers");
+            //for (int i = 0; i < arr1.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr1.GetLength(1); j++)
+            //    {
+            //        arr1[i, j] = int.Parse(Console.ReadLine());
+            //        result[i, j] = arr1[i, j];
+            //    }
+            //}
+            //for (int i = 0; i < arr2.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr2.GetLength(1); j++)
+            //    {
+            //        arr2[i, j] = int.Parse(Console.ReadLine());
+            //        result[i + arr1.GetLength(0), j] = arr2[i, j];
+            //    }
+            //}
+            //for (int i = 0; i < result.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < result.GetLength(1); j++)
+            //    {
+            //        if(result[i, j] != 0)
+            //        {
+            //            Console.WriteLine(result[i, j]);
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region Task 2
+
+            //int[,] TwoDimArray = new int[2, 3];
+            //int sum = 0;
+            //Console.WriteLine("Enter " + (TwoDimArray.GetLength(0) * TwoDimArray.GetLength(1)) + " Numbers");
+            //for (int i = 0; i < TwoDimArray.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < TwoDimArray.GetLength(1); j++)
+            //    {
+            //        TwoDimArray[i, j] = int.Parse(Console.ReadLine());
+            //        sum += TwoDimArray[i, j];
+            //    }
+            //}
+            //Console.WriteLine("Sum is " + sum);
+            //Console.WriteLine("Average is " + ((float)sum /(float) TwoDimArray.Length));
+            #endregion
+
+            #region Task 3
+
+
+            //string str = Console.ReadLine();
+            //int[] arrstr = new int[str.Length];
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    for (int j = 0; j < str.Length; j++)
+            //    {
+            //        if (str[i] == str[j])
+            //        {
+            //            arrstr[i]++;
+            //        }
+            //    }
+            //    Console.WriteLine(str[i] +" : "+ arrstr[i]);
+            //}
+            #endregion
+
+            #region Task 4
+
+
+            //string str = Console.ReadLine();
+            //char[] arrstr = new char[str.Length];
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            //        arrstr[i] = str[i];
+
+            //}
+
+            //for (int i = 0; i < arrstr.Length; i++)
+            //{
+            //    if (arrstr[i]!='\0')
+            //        Console.Write(arrstr[i]);
+            //}
+            #endregion
+
+            #region Task 5
+
+
+            //List<float> list = new List<float>();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    list.Add(float.Parse(Console.ReadLine()));
+            //}            
+            //list.Sort();
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    Console.WriteLine(list[i]);
+            //}
+            #endregion
+
+            #region Task 6
+
+
+            //Dictionary<int, string> student = new Dictionary<int, string>();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    student.Add(i+1, Console.ReadLine());
+            //}
+            //for (int i = 0; i < student.Count; i++)
+            //{
+            //    Console.WriteLine(student.ElementAt(i).Key + " : " + student.ElementAt(i).Value);
+            //}
+            #endregion
+
+
+            #region Task 7
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 5)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+
+            //}
+            // 0 1 2 3 4
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 5)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+
+            //}
+            // 0 1 2 3 4 6 7 8 9
+            #endregion
+
+
+            #region Task 8
+
+
+            //Stack<string> myStack = new Stack<string>();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    myStack.Push(Console.ReadLine());
+            //}
+            //foreach (string item in myStack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //Queue<string> myQueue = new Queue<string>();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    myQueue.Enqueue(Console.ReadLine());
+            //}
+            //foreach (string item in myQueue)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+
+            #region Task 9
+
+            //int[][] JaggedArray = new int[5][];
+            //JaggedArray[0] = new int[] { 1, 2, 3, 4, 5 };
+            //JaggedArray[1] = new int[] { 6, 7, 8 };
+            //JaggedArray[2] = new int[] { 9, 10 };
+            //JaggedArray[3] = new int[] { 11 };
+            //JaggedArray[4] = new int[] { 12, 13, 14, 15, 16, 17 };
+
+            //for (int i = 0; i < JaggedArray.Length; i++)
+            //{
+            //    for (int j = 0; j < JaggedArray[i].Length; j++)
+            //    {
+            //        Console.WriteLine(JaggedArray[i][j]);
+            //    }
+            //}
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
